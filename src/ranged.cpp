01553e8d9dc34f41f75ba6f1be200a104a752154
Drew4484 <57647637+Drew4484@users.noreply.github.com>
2025-01-02 13:52:32 -0800

    Change flag name

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -4258,7 +4258,7 @@ bool gunmode_checks_weapon( avatar &you, const map &m, std::vector<std::string> 
         const bool v_mountable = static_cast<bool>( m.veh_at(
                                      you.pos_bub() ).part_with_feature( "MOUNTABLE",
                                              true ) &&
-                !u.has_trait("ACTIVE_MECH") );
+                !u.has_trait("ACTIVE_MECH_WEAPON") );
         bool t_mountable = m.has_flag_ter_or_furn( ter_furn_flag::TFLAG_MOUNTABLE, you.pos_bub() );
         if( !t_mountable && !v_mountable ) {
             messages.push_back( string_format(
@@ -4270,3 +4270,4 @@ bool gunmode_checks_weapon( avatar &you, const map &m, std::vector<std::string> 
 
     return result;
 }
+F
