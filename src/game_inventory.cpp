30429dfdd963949ba8ed6bc2b0d1dc696a21c05e
juur <juur@users.noreply.github.com>
2025-01-17 20:47:29 +0000

    wip: fix legacy

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -2800,7 +2800,7 @@ bool game_menus::inv::compare_items( const item &first, const item &second,
     return action == "CONFIRM";
 }
 
-void game_menus::inv::compare( const std::optional<tripoint> &offset )
+void game_menus::inv::compare( const std::optional<tripoint_rel_ms> &offset )
 {
     avatar &you = get_avatar();
     you.inv->restack( you );
